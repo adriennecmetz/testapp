a4041086339a1ec61d74a2f082d48f94a1abc4e5
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-02-25 15:07:38 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import Lyrics from './Lyrics';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <img
-            alt="Logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/7/71/Speaker_Icon.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{' '}
-          Lyrics App
-        </Navbar.Brand>
-      </Navbar>
-      <Lyrics />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
