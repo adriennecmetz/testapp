ab5ff2829fd6dd82dbd6cf3b20eef51a95e16326
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-02-26 09:21:24 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [artworkList, setArtworkList] = useState([]);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchTerm}`
+      );
+      const data = await response.json();
+      const objectIDs = data.objectIDs.slice(0, 10);
+      const artworkData = await Promise.all(
+        objectIDs.map(async (id) => {
+          const response = await fetch(
+            `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+          );
+          const data = await response.json();
+          return data;
+        })
+      );
+      setArtworkList(artworkData);
+      setSearchTerm("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Metropolitan Museum of Art API Example</h1>
+      <form onSubmit={handleSearch}>
+        <label htmlFor="searchInput">Search for artwork:</label>
+        <input
+          type="text"
+          id="searchInput"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="artwork-list">
+        {artworkList.map((artwork) => (
+          <div key={artwork.objectID} className="artwork-card">
+            <img
+              src={artwork.primaryImageSmall}
+              alt={artwork.title}
+              className="artwork-image"
+            />
+            <h2>{artwork.title}</h2>
+            <p>{artwork.artistDisplayName}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
